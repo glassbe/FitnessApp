1767be2f55226089bd15813facd8b77a6ef21f9f
michaelOpehij <65280101+michaelOpehij@users.noreply.github.com>
2020-08-25 18:06:22 +0200

    20200825_StatusUpdateRepo_V1_DateHelper

--- app/src/main/java/com/example/fitnessapp/db/DAO/StatusUpdateDAO.java
+++ app/src/main/java/com/example/fitnessapp/db/DAO/StatusUpdateDAO.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapp.db.DAO;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -9,6 +10,7 @@ import androidx.room.Update;
 
 import com.example.fitnessapp.db.Entity.StatusUpdate;
 
+import java.util.Date;
 import java.util.List;
 
 @Dao
@@ -23,7 +25,24 @@ public interface StatusUpdateDAO {
     @Delete
     public void deleteStatusUpdate(StatusUpdate update);
 
-    @Query("SELECT * FROM StatusUpdate WHERE userId = :userId")
-    public List<StatusUpdate> getStatusUpdatesForUser(int userId);
+    @Query("SELECT * FROM StatusUpdate WHERE id = :id")
+    public StatusUpdate getStatusUpdateById(long id);
 
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail")
+    public List<StatusUpdate> getStatusUpdatesForUser(String userMail);
+
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail")
+    public LiveData<List<StatusUpdate>> getStatusUpdatesForUserAsync(String userMail);
+
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail AND timestamp > :from AND timestamp < :until")
+    public LiveData<List<StatusUpdate>> getStatusUpdateForUserFromUntilAsync(String userMail, Date from, Date until);
+
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail AND timestamp > :from AND timestamp < :until")
+    public List<StatusUpdate> getStatusUpdateForUserFromUntil(String userMail, Date from, Date until);
+
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail AND timestamp > :from AND timestamp < :until LIMIT 1")
+    public LiveData<StatusUpdate> getStatusUpdateForUserFromUntilFirstAsync(String userMail, Date from, Date until);
+
+    @Query("SELECT * FROM StatusUpdate WHERE userMail = :userMail AND timestamp > :from AND timestamp < :until LIMIT 1")
+    public StatusUpdate getStatusUpdateForUserFromUntilFirst(String userMail, Date from, Date until);
 }
